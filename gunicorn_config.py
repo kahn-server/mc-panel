43f49fc5b21cc22a804826cd7281fb7dea4826ad
kahn-server <18932378820@sina.cn>
2026-09-24 23:49:37 +0800

    chore: drop gunicorn_config.py (preinstall generates it per frp mode)

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-bind = "0.0.0.0:8080"
-workers = 1
-worker_class = "eventlet"
-certfile = "certs/cert.pem"
-keyfile = "certs/key.pem"
-proxy_protocol = True
-proxy_allow_ips = "127.0.0.1"
-accesslog = "-"
